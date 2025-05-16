d87cd5ecea11a086f03b686951ac7d3a805bd2a7
achraf <achraf.tata2004@gmail.com>
2025-05-16 22:34:11 +0100

    test with users

--- springboot-bank/src/main/java/org/example/springbootbank/SpringbootBankApplication.java
+++ springboot-bank/src/main/java/org/example/springbootbank/SpringbootBankApplication.java
@@ -1,6 +1,9 @@
 package org.example.springbootbank;
 
+import org.example.springbootbank.entities.AccountStatus;
+import org.example.springbootbank.entities.CurrentAccount;
 import org.example.springbootbank.entities.Customer;
+import org.example.springbootbank.entities.SavingAccount;
 import org.example.springbootbank.repository.AccountOperationRepository;
 import org.example.springbootbank.repository.BankAccountRepository;
 import org.example.springbootbank.repository.CustomerRepository;
@@ -9,6 +12,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Date;
 import java.util.stream.Stream;
 
 @SpringBootApplication
@@ -22,12 +26,33 @@ public class SpringbootBankApplication {
 							BankAccountRepository bankAccountRepository,
 							AccountOperationRepository accountOperationRepository) {
 		return args -> {
-			Stream.of("Hassan", "Yassine", "Omar").forEach(name -> {
+			Stream.of("khadija", "yassir", "oma").forEach(name -> {
 				Customer customer = new Customer();
 				customer.setId(name);
 				customer.setName(name);
 				customer.setEmail(name + "@gmail.com");
 				customerRepository.save(customer);
+			});
+
+			customerRepository.findAll().forEach(c -> {
+				CurrentAccount currentAccount = new CurrentAccount();
+				currentAccount.setId(c.getId());
+				currentAccount.setBalance(Math.random() * 90000);
+				currentAccount.setCreatedAt(new Date());
+				currentAccount.setCustomer(c);
+				currentAccount.setOverDraft(9000);
+				currentAccount.setStatus(AccountStatus.CREATED);
+				currentAccount.setCurrency("MAD");
+				bankAccountRepository.save(currentAccount);
+//				SavingAccount savingAccount = new SavingAccount();
+//				savingAccount.setId(c.getId());
+//				savingAccount.setBalance(Math.random() * 90000);
+//				savingAccount.setCreatedAt(new Date());
+//				savingAccount.setCustomer(c);
+//				savingAccount.setInterestRate(5.5);
+//				savingAccount.setStatus(AccountStatus.CREATED);
+//				savingAccount.setCurrency("MAD");
+//				bankAccountRepository.save(savingAccount);
 			});
 		};
 	}
